09684eac10040959ea07b6922ee31518f9117e6f
VarunNightRider <69237458+VarunNightRider@users.noreply.github.com>
2020-08-05 14:59:39 +0530

    Subtraction Module added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -3,4 +3,7 @@ class Calculator{
   public void add(int a, int b){
     System.out.println("Sum = "+(a+b));
   }
+  public void dif(int a, int b){
+    System.out.println("Difference = "+(a-b));
+  }
   }
